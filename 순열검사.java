390eb91142664eb68418ae28051b8a22a396a776
yybmion <yyb400@ajou.ac.kr>
2022-12-05 01:37:26 +0900

    re-순열검사

--- 순열검사.java
+++ 순열검사.java
@@ -1,5 +1,12 @@
 import java.util.Arrays;
 
+
+// 길이가 n인 배열에 1부터 n까지 숫자가 중복 없이 한 번씩 들어 있는지를 확인하려고 합니다.
+
+// 1부터 n까지 숫자가 중복 없이 한 번씩 들어 있는 경우 true를, 
+
+// 아닌 경우 false를 반환하도록 함수 solution을 완성해주세요.
+
 public class 순열검사 {
     public boolean solution(int[] arr){
         int[] answer =new int[arr.length];
