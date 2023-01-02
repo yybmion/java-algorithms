db3432986add3bb0d12a94133223d4fabadd4647
yybmion <yyb400@ajou.ac.kr>
2023-01-03 01:34:44 +0900

    7-2 문자열 내 마음대로 정렬

---
+++ java-collection1/String_Sort.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class String_Sort {
+    public String[] solution(String[] strings,int n){
+        Arrays.sort(strings,(s1, s2) -> {
+            if(s1.charAt(n)==s2.charAt(n)) return s1.compareTo(s2);
+            return s1.charAt(n)-s2.charAt(n);
+        });
+        return strings;
+    }
+}
